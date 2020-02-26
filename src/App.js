031d0956f092de1bd89da9e76fc10c40d1892a9a
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-02-26 02:54:48 +0100

    Adds Context in order to choose the theme of the site

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { Router, Link } from '@reach/router';
 import SearchParams from './SearchParams';
 import Details from './Details';
+import ThemeContext from './ThemeContext';
 
 const App = () => {
+	const themeHook = useState('purple');
 	return (
-		<div>
-			<header>
-				<Link to="/">Adopt Me!</Link>
-			</header>
-			<Router>
-				<SearchParams path="/" />
-				<Details path="/details/:id" />
-			</Router>
-		</div>
+		<React.StrictMode>
+			<ThemeContext.Provider value={themeHook}>
+				<header>
+					<Link to="/">Adopt Me!</Link>
+				</header>
+				<Router>
+					<SearchParams path="/" />
+					<Details path="/details/:id" />
+				</Router>
+			</ThemeContext.Provider>
+		</React.StrictMode>
 	);
 };
 
